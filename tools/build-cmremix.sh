f097c2918bb185044114c73c42770bffd71aaf1a
ZION959 <zionmtran43@gmail.com>
2016-12-30 20:40:23 -0700

    build cmremix 7.1.1_r6 and restart v1.0

--- tools/build-cmremix.sh
+++ tools/build-cmremix.sh
@@ -69,8 +69,8 @@ usage() {
 
 
 # CMREMIX version
-export CMREMIX_VERSION_MAJOR="Nougat_7.1.0_r7"
-export CMREMIX_VERSION_MINOR="v6.0"
+export CMREMIX_VERSION_MAJOR="Nougat_7.1.1_r6"
+export CMREMIX_VERSION_MINOR="v1.0"
 export CMREMIX_VERSION_MAINTENANCE="Official"
 export CMREMIX_KERNEL_VERSION_MAINTENANCE="ZION959"
 # Acceptable maintenance versions are; Stable, Official, Nightly or Unofficial
